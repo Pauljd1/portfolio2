cc4bbaa6e35bb3d1bfaa8321722d0a843b011e58
Pauljd <pauljd@63e1adb99d77>
2025-06-01 20:55:41 +0100

    Fix project image display with sizing constraints

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -159,6 +159,7 @@ export const ProjectsSection = () => {
                       alt={project.title}
                       className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
                       loading="lazy"
+                      style={{ maxHeight: "500px", objectFit: "contain" }}
                     />
                   </div>
                 </div>
